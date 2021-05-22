d4c2e5b2d9cba20dd1fe36c387b62a27549c0d5f
souben <sbannoun@gmail.com>
2021-05-22 20:03:11 +0100

    add variables section

---
+++ variable/example.ts
@@ -0,0 +1,10 @@
+function varletFunc() {
+    var myStringValuevar = "varStringValue";
+    let myStringValueLet = "letStringValue";
+    if(true){
+        // var myStringValuevar = 123; causes an error , comment to see
+        let myStringValueLet = "llll";
+    }
+    console.log(myStringValueLet);
+    console.log(myStringValuevar);
+}
---
+++ variable/varFunc.ts
@@ -0,0 +1,22 @@
+/*
+One issue with var is that the location of a variable makes it unpredictable. A variable declared using var is function-scoped when declared inside a function, but global-scoped when declared outside of a function. Also, var does not stop you from redefining the same variable, which overrides the initial declaration or initialization.
+
+VAR IS FUNCTION-SCOPED WHEN DECLARED INSIDE A FUNCTION,
+BUT GLOBAL-SCOPED WHEN DECLARED OUTSIDE OF A FUNCTION.
+*/
+
+
+
+function varfunc(){
+    var x = "llll";
+    if(true){
+        var x = "9999";
+        if(true){
+            var y = "hola man , I like this shit ...";
+        }
+    }
+    console.log(x);
+    console.log(y);
+}
+varfunc();
+
